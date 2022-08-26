c2641e07857698f7f0bd058ab4d0d892c102ba80
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 23:29:47 +0900

    design row

--- front/app/src/components/HouseKeepRow.tsx
+++ front/app/src/components/HouseKeepRow.tsx
@@ -1,5 +1,50 @@
-import { Checkbox } from "@chakra-ui/react";
+import {
+  Checkbox,
+  Flex,
+  NumberInput,
+  NumberInputField,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-const HouseKeepRow = () => <Checkbox />;
+const HouseKeepRow = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [value, setValue] = useState("10000");
+
+  const format = (val: string | number) => {
+    if (typeof val == "string") {
+      val = parseInt(val);
+    }
+    if (isNaN(val)) {
+      val = 0;
+    }
+    return `¥` + Intl.NumberFormat("ja-JP").format(val);
+  };
+
+  return (
+    <Flex align="center" gap={4}>
+      <Checkbox
+        colorScheme="brand"
+        onChange={() => setIsChecked((prev) => !prev)}
+      />
+      <Text width="200px" color={isChecked ? "inherit" : "gray.400"}>
+        家賃
+      </Text>
+      <NumberInput
+        width="300px"
+        variant="flushed"
+        focusBorderColor="brand.500"
+        onChange={(valueString) => setValue(valueString)}
+        value={value}
+        min={0}
+        format={format}
+        step={1000}
+        isDisabled={!isChecked}
+      >
+        <NumberInputField />
+      </NumberInput>
+    </Flex>
+  );
+};
 
 export default HouseKeepRow;
