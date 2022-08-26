581c00976445b81c2f3c2f342b410e96f2ce5b0f
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 19:07:23 +0900

    design search panel

--- front/app/src/components/AdviceSearchPanel.tsx
+++ front/app/src/components/AdviceSearchPanel.tsx
@@ -1,8 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Checkbox, CheckboxGroup, Input } from "@chakra-ui/react";
 
 const AdviceSearchPanel = () => (
-  <Box bg="brand.100" height="195px" width="80%" borderRadius="5px" p={4}>
+  <Box bg="brand.100" width="80%" borderRadius="5px" p={4}>
     検索
+    <Box mt={4} ml={4}>
+      ユーザーの属性
+      <CheckboxGroup colorScheme="brand">
+        <Checkbox mx={4} borderColor="white">
+          女性
+        </Checkbox>
+        <Checkbox mx={4} borderColor="white">
+          男性
+        </Checkbox>
+        <Checkbox mx={4} borderColor="white">
+          学生
+        </Checkbox>
+        <Checkbox mx={4} borderColor="white">
+          新社会人
+        </Checkbox>
+        <Checkbox mx={4} borderColor="white">
+          在宅ワーク
+        </Checkbox>
+      </CheckboxGroup>
+    </Box>
+    <Box mt={4} ml={4}>
+      フリーワード　
+      <Input
+        width="30%"
+        variant="flushed"
+        focusBorderColor="brand.500"
+        borderColor="white"
+        placeholder="入力してください"
+      />
+    </Box>
+    <Box my={4} ml="auto" textAlign="center">
+      <Button colorScheme="brand">検索する</Button>
+    </Box>
   </Box>
 );
 
