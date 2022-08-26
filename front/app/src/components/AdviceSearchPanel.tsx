acfc8a7f1807a064bb60ed34f3c9db1602570b94
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 20:16:14 +0900

    use FormControl

--- front/app/src/components/AdviceSearchPanel.tsx
+++ front/app/src/components/AdviceSearchPanel.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Checkbox, CheckboxGroup, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { FormEvent, useState } from "react";
 
 const AdviceSearchPanel = () => {
@@ -24,55 +32,60 @@ const AdviceSearchPanel = () => {
       検索
       <form onSubmit={handleSubmit}>
         <Box mt={4} ml={4}>
-          ユーザーの属性
-          <CheckboxGroup colorScheme="brand">
-            <Checkbox
-              mx={4}
-              borderColor="white"
-              onChange={() => toggleChecked("女性")}
-            >
-              女性
-            </Checkbox>
-            <Checkbox
-              mx={4}
-              borderColor="white"
-              onChange={() => toggleChecked("男性")}
-            >
-              男性
-            </Checkbox>
-            <Checkbox
-              mx={4}
-              borderColor="white"
-              onChange={() => toggleChecked("学生")}
-            >
-              学生
-            </Checkbox>
-            <Checkbox
-              mx={4}
-              borderColor="white"
-              onChange={() => toggleChecked("新社会人")}
-            >
-              新社会人
-            </Checkbox>
-            <Checkbox
-              mx={4}
-              borderColor="white"
-              onChange={() => toggleChecked("在宅ワーク")}
-            >
-              在宅ワーク
-            </Checkbox>
-          </CheckboxGroup>
+          <FormControl as="fieldset">
+            <FormLabel as="legend">ユーザーの属性</FormLabel>
+            <CheckboxGroup colorScheme="brand">
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("女性")}
+              >
+                女性
+              </Checkbox>
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("男性")}
+              >
+                男性
+              </Checkbox>
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("学生")}
+              >
+                学生
+              </Checkbox>
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("新社会人")}
+              >
+                新社会人
+              </Checkbox>
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("在宅ワーク")}
+              >
+                在宅ワーク
+              </Checkbox>
+            </CheckboxGroup>
+          </FormControl>
         </Box>
         <Box mt={4} ml={4}>
-          フリーワード　
-          <Input
-            width="30%"
-            variant="flushed"
-            focusBorderColor="brand.500"
-            borderColor="white"
-            placeholder="入力してください"
-            onChange={(event) => setFreeWord(event.currentTarget.value)}
-          />
+          <FormControl>
+            <FormLabel>フリーワード</FormLabel>
+            <Input
+              width="30%"
+              ml={4}
+              variant="flushed"
+              focusBorderColor="brand.500"
+              borderColor="white"
+              placeholder="入力してください"
+              onChange={(event) => setFreeWord(event.currentTarget.value)}
+            />
+          </FormControl>
         </Box>
         <Box my={4} ml="auto" textAlign="center">
           <Button colorScheme="brand" type="submit">
