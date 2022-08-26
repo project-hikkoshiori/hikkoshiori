5df24b1eb43c50028448923541cf4caee2793698
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 20:09:54 +0900

    send form base

--- front/app/src/components/AdviceSearchPanel.tsx
+++ front/app/src/components/AdviceSearchPanel.tsx
@@ -1,42 +1,87 @@
 import { Box, Button, Checkbox, CheckboxGroup, Input } from "@chakra-ui/react";
+import { FormEvent, useState } from "react";
 
-const AdviceSearchPanel = () => (
-  <Box bg="brand.100" width="80%" borderRadius="5px" p={4}>
-    検索
-    <Box mt={4} ml={4}>
-      ユーザーの属性
-      <CheckboxGroup colorScheme="brand">
-        <Checkbox mx={4} borderColor="white">
-          女性
-        </Checkbox>
-        <Checkbox mx={4} borderColor="white">
-          男性
-        </Checkbox>
-        <Checkbox mx={4} borderColor="white">
-          学生
-        </Checkbox>
-        <Checkbox mx={4} borderColor="white">
-          新社会人
-        </Checkbox>
-        <Checkbox mx={4} borderColor="white">
-          在宅ワーク
-        </Checkbox>
-      </CheckboxGroup>
+const AdviceSearchPanel = () => {
+  const [checked, setChecked] = useState<string[]>([]);
+  const [freeWord, setFreeWord] = useState("");
+
+  const toggleChecked = (value: string) => {
+    if (checked.indexOf(value) == -1) {
+      setChecked((prev) => [...prev, value]);
+    } else {
+      setChecked((prev) => prev.filter((v) => v != value));
+    }
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // TODO: いい感じに処理
+    console.log(`checked: ${checked}, freeword: ${freeWord}`);
+  };
+
+  return (
+    <Box bg="brand.100" width="80%" borderRadius="5px" p={4}>
+      検索
+      <form onSubmit={handleSubmit}>
+        <Box mt={4} ml={4}>
+          ユーザーの属性
+          <CheckboxGroup colorScheme="brand">
+            <Checkbox
+              mx={4}
+              borderColor="white"
+              onChange={() => toggleChecked("女性")}
+            >
+              女性
+            </Checkbox>
+            <Checkbox
+              mx={4}
+              borderColor="white"
+              onChange={() => toggleChecked("男性")}
+            >
+              男性
+            </Checkbox>
+            <Checkbox
+              mx={4}
+              borderColor="white"
+              onChange={() => toggleChecked("学生")}
+            >
+              学生
+            </Checkbox>
+            <Checkbox
+              mx={4}
+              borderColor="white"
+              onChange={() => toggleChecked("新社会人")}
+            >
+              新社会人
+            </Checkbox>
+            <Checkbox
+              mx={4}
+              borderColor="white"
+              onChange={() => toggleChecked("在宅ワーク")}
+            >
+              在宅ワーク
+            </Checkbox>
+          </CheckboxGroup>
+        </Box>
+        <Box mt={4} ml={4}>
+          フリーワード　
+          <Input
+            width="30%"
+            variant="flushed"
+            focusBorderColor="brand.500"
+            borderColor="white"
+            placeholder="入力してください"
+            onChange={(event) => setFreeWord(event.currentTarget.value)}
+          />
+        </Box>
+        <Box my={4} ml="auto" textAlign="center">
+          <Button colorScheme="brand" type="submit">
+            検索する
+          </Button>
+        </Box>
+      </form>
     </Box>
-    <Box mt={4} ml={4}>
-      フリーワード　
-      <Input
-        width="30%"
-        variant="flushed"
-        focusBorderColor="brand.500"
-        borderColor="white"
-        placeholder="入力してください"
-      />
-    </Box>
-    <Box my={4} ml="auto" textAlign="center">
-      <Button colorScheme="brand">検索する</Button>
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default AdviceSearchPanel;
