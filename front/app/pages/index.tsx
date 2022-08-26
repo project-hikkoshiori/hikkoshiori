da82f1cd641c7e72d99df4ea9458572e92ce98c3
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 11:52:17 +0900

    show image on top

--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import useSWR from "swr";
 import { Fetcher } from "../src/utils/fetcher";
+import topImageSrc from "../public/top.png";
 
 const Home: NextPage = () => {
   const { data } = useSWR(`${process.env.NEXT_PUBLIC_SERVER_PATH}`, Fetcher);
   if (!data) {
     return <p>no data found...</p>;
   }
-  return <p>data found!!!</p>;
+  return <Image src={topImageSrc} alt="ひっこしおり" />;
 };
 
 export default Home;
