b93e6626b312edbea09d9c20671d5475d76672bf
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 20:28:49 +0900

    Revert "add advicee page"
    This reverts commit 44c313ad0bc8ba2902e45fde21a34bab2c3de6ed.

--- front/app/pages/advice/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-import type { NextPage } from "next";
-const Advice: NextPage = () => {
-  return <Heading>みんなのアドバイス</Heading>;
-};
-
-export default Advice;
