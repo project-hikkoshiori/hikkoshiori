dc4f2ff08ae823d6490928a81c9f9be69aa8e9b3
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 20:24:55 +0900

    Revert "move advices to center"
    This reverts commit cd37b7fe7e9cdfa0ad7ed8158d4bb48c83de29ad.

--- front/app/pages/advice/index.tsx
+++ front/app/pages/advice/index.tsx
@@ -7,7 +7,7 @@ const Advice: NextPage = () => {
   return (
     <Flex align="center" flexDir="column">
       <Heading>みんなのアドバイス</Heading>
-      <Flex wrap="wrap" justify="center">
+      <Flex wrap="wrap">
         {advices.map((advice) => (
           <AdviceCell key={advice} />
         ))}
