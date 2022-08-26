44c313ad0bc8ba2902e45fde21a34bab2c3de6ed
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-08-26 17:15:35 +0900

    add advicee page

---
+++ front/app/pages/advice/index.tsx
@@ -0,0 +1,7 @@
+import { Heading } from "@chakra-ui/react";
+import type { NextPage } from "next";
+const Advice: NextPage = () => {
+  return <Heading>みんなのアドバイス</Heading>;
+};
+
+export default Advice;
