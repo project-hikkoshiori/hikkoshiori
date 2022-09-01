07828d5e10793745f7579c1d38212b4507184cc7
ogiwelcome <ogi.fordevelopment@gmail.com>
2022-09-01 17:47:34 +0900

    検索と結果表示を追加

--- front/app/src/components/properties/PropertySearchSection.tsx
+++ front/app/src/components/properties/PropertySearchSection.tsx
@@ -1,8 +1,31 @@
-import { Box, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Flex,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { Property } from "../../utils/types";
+import { BookmarkWindow } from "./BookmarkWindow";
 import { PropertySearchMap } from "./PropertySearchMap";
 
 export const PropertySearchSection = () => {
+  const [checked, setChecked] = useState<string[]>([]);
+  const toggleChecked = (value: string) => {
+    if (checked.indexOf(value) == -1) {
+      setChecked((prev) => [...prev, value]);
+    } else {
+      setChecked((prev) => prev.filter((v) => v != value));
+    }
+  };
+  const handleSubmit = () => {
+    // responseがpropertiesになる
+    console.log(`checked: ${checked}`);
+  };
   // mock
   const properties: Property[] = [
     {
@@ -48,7 +71,49 @@ export const PropertySearchSection = () => {
   ];
   return (
     <Flex>
-      ここに検索フィルターとかが入る
+      <Box w="45%">
+        <Stack alignContent="left">
+          <Text fontWeight="semibold">検索条件</Text>
+          <CheckboxGroup colorScheme="brand">
+            <Stack direction="row" p="2">
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("南向き")}
+              >
+                南向き
+              </Checkbox>
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("二階以上")}
+              >
+                二階以上
+              </Checkbox>
+              <Checkbox
+                mx={4}
+                borderColor="white"
+                onChange={() => toggleChecked("宅配ボックス")}
+              >
+                宅配ボックス
+              </Checkbox>
+            </Stack>
+          </CheckboxGroup>
+          <Button type="submit" colorScheme="brand" onClick={handleSubmit}>
+            検索
+          </Button>
+        </Stack>
+        <Text fontWeight="semibold" pt="5">
+          検索結果
+        </Text>
+        <Box overflowY="auto" maxH="800">
+          {properties.map((p: Property) => (
+            <Box key={p.id} p="5">
+              <BookmarkWindow property={p} />
+            </Box>
+          ))}
+        </Box>
+      </Box>
       <Spacer />
       <Box w="50%">
         <PropertySearchMap properties={properties} />
