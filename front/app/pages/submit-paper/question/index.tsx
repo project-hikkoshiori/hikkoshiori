2a625e41acab20b13990dfe2272de47d0c1f8ed7
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-01 18:15:00 +0900

    show result component

--- front/app/pages/submit-paper/question/index.tsx
+++ front/app/pages/submit-paper/question/index.tsx
@@ -7,21 +7,35 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import WeitingResult from "../../../src/components/submit-paper/WeitingResult";
 
 const PaperQuestion: NextPage = () => {
+  const [isWaitingResult, setIsWeitingResult] = useState(false);
   return (
     <VStack my={4}>
       <Heading my={8}>提出書類フローチャート</Heading>
-      <Text mb={40}>Q1. なんかいい感じに質問してください？</Text>
-      <HStack>
-        <Button colorScheme="brand" height="64px" width="400px">
-          いいえ
-        </Button>
-        <Button colorScheme="brand" height="64px" width="400px">
-          はい
-        </Button>
-      </HStack>
-      <Progress height="20px" width="500px" value={20} colorScheme="brand" />
+      {isWaitingResult ? (
+        <WeitingResult />
+      ) : (
+        <>
+          <Text mb={40}>Q1. なんかいい感じに質問してください？</Text>
+          <HStack>
+            <Button colorScheme="brand" height="64px" width="400px">
+              いいえ
+            </Button>
+            <Button colorScheme="brand" height="64px" width="400px">
+              はい
+            </Button>
+          </HStack>
+          <Progress
+            height="20px"
+            width="500px"
+            value={20}
+            colorScheme="brand"
+          />
+        </>
+      )}
     </VStack>
   );
 };
