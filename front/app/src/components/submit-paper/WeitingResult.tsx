dbf8bcd234fb6f21b1a2cb0dd3ec0ccd31cf0de8
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-01 18:14:51 +0900

    add result component

---
+++ front/app/src/components/submit-paper/WeitingResult.tsx
@@ -0,0 +1,10 @@
+import { Text, VStack } from "@chakra-ui/react";
+
+const WeitingResult = () => (
+  <VStack align="center" p={2} width="500px">
+    <Text>終了！</Text>
+    <Text>診断結果を生成中...</Text>
+  </VStack>
+);
+
+export default WeitingResult;
