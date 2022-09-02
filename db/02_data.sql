2267ff8db6247ded0ed6ded17834117461e19878
ogiwelcome <ogi.fordevelopment@gmail.com>
2022-09-02 16:01:20 +0900

    timestampいれた

--- db/02_data.sql
+++ db/02_data.sql
@@ -7,20 +7,20 @@ insert into advices values(
     '9046cca2-083d-364a-9704-51a37b98e4e8',
     '81f981b2-bdfa-4b98-b1a3-b4669f948a12',
     '部屋干し対策としてAttack Zeroが無臭でいいという話があります。',
-    '',
+    '2022-03-01 10:00:00',
     'invalid'
 );
 insert into advices values(
     '34aadc65-d971-6311-4565-b70f5deb6d07',
     '81f981b2-bdfa-4b98-b1a3-b4669f948a12',
     '京都は除湿器がとても大事。',
-    '',
+    '2022-03-01 10:00:00',
     'invalid'
 );
 insert into advices values(
     'a6924e50-ed0d-1d0c-4935-cddd3e5e5548',
     '81f981b2-bdfa-4b98-b1a3-b4669f948a12',
     '(=^・・^=)',
-    '',
+    '2022-03-01 10:00:00',
     'invalid'
 );
