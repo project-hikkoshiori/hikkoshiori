00aa8c52e72f4f92472be0f23b27be9f492d9ddd
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-16 15:41:44 +0900

    bookmark link available

--- front/app/pages/properties/index.tsx
+++ front/app/pages/properties/index.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import { Tabs, Tab, TabList, TabPanels, TabPanel } from "@chakra-ui/react";
 import { BookmarkSection } from "../../src/components/properties/BookmarkSection";
 import { PropertySearchSection } from "../../src/components/properties/PropertySearchSection";
 
 const Index: NextPage = () => {
+  const router = useRouter();
+  const [tabIndex, setTabIndex] = useState(0);
+
+  useEffect(() => {
+    if (router.isReady) {
+      setTabIndex(router.query.bookmark == "true" ? 1 : 0);
+    }
+  }, [router]);
+
+  const handleTabsChange = (index: number) => {
+    setTabIndex(index);
+  };
+
   return (
-    <Tabs isFitted mt="50" ml="10" mr="10">
+    <Tabs
+      isFitted
+      mt="50"
+      ml="10"
+      mr="10"
+      index={tabIndex}
+      onChange={handleTabsChange}
+    >
       <TabList>
         <Tab _selected={{ color: "white", bg: "brand.100" }}>物件を探す</Tab>
         <Tab _selected={{ color: "white", bg: "brand.100" }}>
