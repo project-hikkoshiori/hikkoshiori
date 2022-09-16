f087c9071e848704018dec6270f2ce232e71fdc5
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-16 22:16:30 +0900

    fix api

--- server/app/controllers/housekeep.py
+++ server/app/controllers/housekeep.py
@@ -5,14 +5,15 @@ from fastapi import Depends, HTTPException
 from db import get_db
 from sqlalchemy.orm import Session
 
-from models.schemas.housekeep_table import HouseKeepTable
-from models.db.housekeep_table_db import get_user_housekeep_tables
+from models.db.housekeep_column_db import get_user_housekeep_columns
+from models.schemas.housekeep_column import HouseKeepColumnResponse
 class HouseKeepController:
     def __init__(self, app, logger):
-        @app.get("/housekeeps/{user_id}", response_model=List[HouseKeepTable])
+        @app.get("/housekeeps/{user_id}", response_model=List[HouseKeepColumnResponse])
         async def get_housekeeps(user_id: str, db: Session = Depends(get_db)):
             try:
-                result = get_user_housekeep_tables(db, user_id)
+                result = get_user_housekeep_columns(db, user_id)
+                print(result)
             except Exception as e:
                 logger.error(e)
                 raise HTTPException(
