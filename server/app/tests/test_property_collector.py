d896ee96098e70a62dc7fc5480c5c28ef51cadfa
Kenta Shinzato <hoppiece@gmail.com>
2022-09-16 19:21:12 +0900

    テストでlinks がリストのママになっていたのを修正

--- server/app/tests/test_property_collector.py
+++ server/app/tests/test_property_collector.py
@@ -76,4 +76,4 @@ class TestSuumoParser:
 
     def test_image_links(self, suumo_parsers):
         links = suumo_parsers[0].image_links
-        assert links[0].startswith("https://img01.suumo.com/front/gazo/fr/")
+        assert links["間取り図"].startswith("https://img01.suumo.com/front/gazo/fr/")
