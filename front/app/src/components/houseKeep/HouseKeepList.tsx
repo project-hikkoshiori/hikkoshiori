50ac2b9491cfb14f54b72daecb76a78fa25129d2
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-17 21:40:03 +0900

    fix scroll layout

--- front/app/src/components/houseKeep/HouseKeepList.tsx
+++ front/app/src/components/houseKeep/HouseKeepList.tsx
@@ -43,12 +43,13 @@ const HouseKeepList = ({ housekeeps }: Props) => {
   });
 
   return (
-    <VStack align="end" p={2}>
+    <VStack p={2}>
       <VStack
         maxW="700px"
         minW="500px"
         height="calc(100vh - 186px)"
         gap={4}
+        p={8}
         align="left"
         overflow="auto"
         background="linear-gradient(white 0%, white 100%) 0 0/100% 8px,linear-gradient(white 0%, white 100%) bottom/100% 8px,radial-gradient(ellipse 50% 8px at top, rgba(0,0,0,0.2) 0%, rgba(255,255,255,0) 100%) 0 0/100% 8px,radial-gradient(ellipse 50% 8px  at bottom , rgba(0,0,0,0.2) 0%, rgba(255,255,255,0) 100%) bottom/100% 8px"
