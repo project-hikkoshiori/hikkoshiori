8825f9250ba59c011169b8636e063a05c059afeb
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-18 10:42:12 +0900

    use useGetBookmarkedProperties for map

--- front/app/src/components/properties/BookmarkSection.tsx
+++ front/app/src/components/properties/BookmarkSection.tsx
@@ -37,25 +37,6 @@ export const BookmarkSection = () => {
   ) {
     router.push("/users/register");
   }
-  const properties: BookmarkedProperty[] = [
-    {
-      id: "4f66a848-3369-11ed-9208-0242ac160004",
-      location: "千葉県柏市布施974-19",
-      lat: 36.68156,
-      lng: 139.767201,
-      monthly_rent_price: 63000,
-      monthly_maintenance_fee: 0,
-      initial_cost: 0,
-      distance_station_raw: "JR常磐線 北柏駅 徒歩23分\n",
-      house_layout: "3LDK",
-      exclusive_area: 71.21,
-      age_of_building: 42,
-      floor_num: 1,
-      direction: "南",
-      fetched_at: "2022-09-13T05:41:01.682674",
-      user_id: "81f981b2-bdfa-4b98-b1a3-b4669f948a12",
-    },
-  ];
   return (
     <Stack p="5">
       <Flex mt="50" mb="5">
@@ -84,7 +65,7 @@ export const BookmarkSection = () => {
         </ButtonGroup>
       </Flex>
       {mapMode ? (
-        <PropertySearchMap properties={properties} />
+        <PropertySearchMap properties={bookmarkedProperties} />
       ) : (
         <SimpleGrid columns={3} spacing={10}>
           {bookmarkedProperties.map((p: BookmarkedProperty) => (
