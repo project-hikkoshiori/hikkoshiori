0bf945453ce0865703c1e442009a724158751600
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-09-18 15:07:19 +0900

    overflow layout

--- front/app/src/components/properties/BookmarkSection.tsx
+++ front/app/src/components/properties/BookmarkSection.tsx
@@ -96,7 +96,7 @@ export const BookmarkSection = () => {
       {mapMode ? (
         <PropertySearchMap properties={bookmarkedProperties} />
       ) : (
-        <SimpleGrid columns={3} spacing={10}>
+        <SimpleGrid columns={3} spacing={10} overflowY="auto" px={4}>
           {bookmarkedProperties.map((p: BookmarkedProperty) => (
             <BookmarkWindow key={p.id} property={p} />
           ))}
