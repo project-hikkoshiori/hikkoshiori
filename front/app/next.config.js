8ac16e10fa8e4ea17987d8790f006a6cada12837
ogiwelcome <ogi.fordevelopment@gmail.com>
2022-09-18 13:29:30 +0900

    delete-log

--- front/app/next.config.js
+++ front/app/next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const TerserPlugin = require("terser-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     loader: 'akamai',
     path: '',
+  },
+  webpack: (config, options) => {
+    config.optimization.minimize = true;
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+        extractComments: "all",
+      }),
+    ];
+    return config;
   },
 }
 
